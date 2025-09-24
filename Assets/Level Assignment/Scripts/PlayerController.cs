5cf0bc3c5c20f8b0ba11a9dad4ad5c0c4d526e56
Ahamed Maahin Siddiq Rahuman <ahamaasidrah@gmail.com>
2025-09-23 21:21:17 -0600

    Project Added:
    The Level blockout of the small portion of a villa has been successfully added in the repo. This has been done for the Assignment 1's purpose of Technical Art (Term 4). The exterior part has been done with the usage of the created MODULAR ASSETS from MAYA software and imported it here.

---
+++ Assets/Level Assignment/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] public float movementSpeed;
+    public float playerJump;
+    private Rigidbody playerRB;
+    //private bool isPlayerGrounded;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        //isPlayerGrounded = true;
+    }
+
+    void Update()
+    {
+
+        //For Straight, Back, Left and Right movements:
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerRB.position += Vector3.forward * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            playerRB.position += Vector3.back * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            playerRB.position += Vector3.left * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            playerRB.position += Vector3.right * Time.deltaTime * movementSpeed;
+        }
+
+    }
+
+}
