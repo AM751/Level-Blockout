3dfacc0893e70a7175c2ab399b2539847bd457a4
Ahamed Maahin. <ahamaasidrah@gmail.com>
2025-10-21 22:03:32 -0600

    Player Controller:
    Player movements bug has been sorted. The input controls were inverted and now its sorted out and Jump functionality has been added to the player.

--- Assets/Level Assignment/Scripts/PlayerController.cs
+++ Assets/Level Assignment/Scripts/PlayerController.cs
@@ -19,19 +19,24 @@ public class PlayerController : MonoBehaviour
         //For Straight, Back, Left and Right movements:
         if (Input.GetKey(KeyCode.W))
         {
-            playerRB.position += Vector3.forward * Time.deltaTime * movementSpeed;
+            playerRB.position += Vector3.back * Time.deltaTime * movementSpeed;
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            playerRB.position += Vector3.back * Time.deltaTime * movementSpeed;
+            playerRB.position += Vector3.forward * Time.deltaTime * movementSpeed;
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            playerRB.position += Vector3.left * Time.deltaTime * movementSpeed;
+            playerRB.position += Vector3.right * Time.deltaTime * movementSpeed;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            playerRB.position += Vector3.right * Time.deltaTime * movementSpeed;
+            playerRB.position += Vector3.left * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.AddForce(Vector3.up * playerJump, ForceMode.Impulse);
         }
 
     }
